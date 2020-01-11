7205030cd9b9434a787cc7594d4cecaa1220feea
jinsung <wlstjdpark@gmail.com>
2020-01-12 01:08:00 +0900

    점수 추가, 게임 종료 등 추가

--- tetris.js
+++ tetris.js
@@ -1,19 +1,58 @@
-// 디자인 참고
-// https://www.freetetris.org/game.php
-
-let cellArray;
 let boardElement;
+
+let currentBlock;
+let fixedBlockArray;
+let scoreElement;
+let score;
+let timer;
 
 const BOARD_WIDTH = 10;
 const BOARD_HEIGHT = 20;
-const BLOCK_WIDTH = 20;
-const BLOCK_HEIGHT = 20;
+const BLOCK_WIDTH = 30;
+const BLOCK_HEIGHT = 30;
+const GAME_SPEED = 1000;
+
+const KEY_SPACE = 32;
+const KEY_ARROW_LEFT = 37;
+const KEY_ARROW_UP = 38;
+const KEY_ARROW_RIGHT = 39;
+const KEY_ARROW_DOWN = 40;
+window.addEventListener('keydown', (e) => {
+  keyDown(e);
+}, false);
+
+function keyDown(keyEvent) {
+  switch (keyEvent.keyCode) {
+    case KEY_ARROW_LEFT:
+      keyEvent.preventDefault();
+      currentBlock.moveLeft();
+      break;
+    case KEY_ARROW_RIGHT:
+      keyEvent.preventDefault();
+      currentBlock.moveRight();
+      break;
+    case KEY_ARROW_UP:
+      keyEvent.preventDefault();
+      currentBlock.rotate();
+      break;
+    case KEY_ARROW_DOWN:
+      keyEvent.preventDefault();
+      blockDown();
+      break;
+    case KEY_SPACE:
+      keyEvent.preventDefault();
+      while (true) {
+        if (blockDown() === false) {
+          break;
+        }
+      }
+      break;
+  }
+  draw();
+}
 
 class Block {
   constructor() {
-    this.blockClass = '';
-    this.colorType = -1;
-    this.currentRoateIndex = 0;
     this.posArray;
     this.currentPosition;
     this.posX = BOARD_WIDTH / 2 - 2;
@@ -23,9 +62,9 @@ class Block {
 
   getBlockPosition() {
     if (!this.posArray) {
-      return;
+      return [];
     }
-    let result = []
+    let result = [];
     for (let y = 0; y < this.posArray.length; ++y) {
       for (let x = 0; x < this.posArray[y].length; ++x) {
         if (this.posArray[y][x] === 1) {
@@ -81,6 +120,54 @@ class Block {
       this.posArray = old;
     }
   }
+  moveSide(value) {
+    let blockPosition = this.getBlockPosition();
+    if (value === -1) {
+      if (Math.min(...blockPosition.map(x => x[1])) === 0) {
+        return;
+      }
+    } else {
+      if (Math.max(...blockPosition.map(x => x[1])) === BOARD_WIDTH -1) {
+        return;
+      }
+    }
+
+    for (let i = 0; i < blockPosition.length; ++i) {
+      if (fixedBlockArray[blockPosition[i][0]][blockPosition[i][1] + value] !== '') {
+        return;
+      }
+    }
+
+    this.posX += value;
+  }
+
+  moveLeft() {
+    this.moveSide(-1);
+  }
+
+  moveRight() {
+    this.moveSide(1);
+  }
+  tryBlockDown() {
+    const blockPosition = this.getBlockPosition();
+    for (let i = 0; i < blockPosition.length; ++i) {
+      if (blockPosition[i][0] === BOARD_HEIGHT - 1) {
+        return false;
+      }
+      if (fixedBlockArray[blockPosition[i][0] + 1][blockPosition[i][1]] !== '') {
+        return false;
+      }
+    }
+    return true;
+  }
+  blockDown() {
+    if (this.tryBlockDown()) {
+      this.posY += 1;
+      return true;
+    } else {
+      return false;
+    }
+  }
   draw() {
     for (let y = 0; y < this.posArray.length; ++y) {
       for (let x = 0; x < this.posArray[y].length; ++x) {
@@ -92,19 +179,16 @@ class Block {
   }
 }
 
-let blockArray = [];
-let fixedBlockArray = [];
-
 class BlockI extends Block {
   constructor(){
     super();
-    this.color = 'orangered';
+    this.color = '#9BACD2';
     this.shapeSize = 4;
     this.posArray = [
-        [0, 0, 1, 0],
-        [0, 0, 1, 0],
-        [0, 0, 1, 0],
-        [0, 0, 1, 0]
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0]
       ];
   }
 }
@@ -112,7 +196,7 @@ class BlockI extends Block {
 class BlockT extends Block {
   constructor(){
     super();
-    this.color = 'lightgray';
+    this.color = '#FBE6C0';
     this.shapeSize = 3;
     this.posArray = [
         [0, 1, 0, 0],
@@ -126,7 +210,7 @@ class BlockT extends Block {
 class BlockA extends Block {
   constructor(){
     super();
-    this.color = 'skyblue';
+    this.color = '#FF3213';
     this.shapeSize = 3;
     this.posArray = [
         [0, 1, 0, 0],
@@ -140,7 +224,7 @@ class BlockA extends Block {
 class BlockB extends Block {
   constructor(){
     super();
-    this.color = 'green';
+    this.color = '#FF971C';
     this.shapeSize = 3;
     this.posArray = [
         [1, 0, 0, 0],
@@ -154,7 +238,7 @@ class BlockB extends Block {
 class BlockL extends Block {
   constructor(){
     super();
-    this.color = 'violet';
+    this.color = '#FFD500';
     this.shapeSize = 3;
     this.posArray = [
         [1, 0, 0, 0],
@@ -168,7 +252,7 @@ class BlockL extends Block {
 class BlockR extends Block {
   constructor(){
     super();
-    this.color = 'blue';
+    this.color = '#0341AE';
     this.shapeSize = 3;
     this.posArray = [
         [0, 1, 0, 0],
@@ -182,7 +266,7 @@ class BlockR extends Block {
 class BlockM extends Block {
   constructor(){
     super();
-    this.color = 'darkolivegreen';
+    this.color = '#72CB3B';
     this.shapeSize = 2;
     this.posArray = [
         [1, 1, 0, 0],
@@ -206,16 +290,14 @@ function setFixedBlock(e, color) {
 }
 
 function initBoard() {
-  console.log('initBoard');
   boardElement = [];
-  cellArray = [];
-  const div = document.createElement('div');
+  let div = document.createElement('div');
+  div.classList.add('board');
   for (let i = 0; i < BOARD_HEIGHT; ++i) {
     let rElement = document.createElement('div');
     rElement.style.width = (BOARD_WIDTH * BLOCK_WIDTH)+'px';
     rElement.style.height = BLOCK_HEIGHT + 'px';
     boardElement.push([]);
-    cellArray.push([]);
 
     for (let j = 0; j < BOARD_WIDTH; ++j) {
       let cellElement = document.createElement('div');
@@ -225,21 +307,40 @@ function initBoard() {
 
       rElement.appendChild(cellElement);
       boardElement[i].push(cellElement);
-      cellArray[i].push(0);
     }
     div.appendChild(rElement);
   }
   document.querySelector('body').appendChild(div);
 }
 
-function getBlockWidth(block) {
-  let maxX = 0;
-  block.forEach(x => maxX = (maxX < x[0] ? x[0] : maxX) + 1);
-  return maxX;
+function initScore() {
+  score = 0;
+  scoreElement = document.createElement('div');
+  scoreElement.classList.add('score');
+  scoreElement.textContent = `Score: ${score}`;
+  document.querySelector('.board').appendChild(scoreElement);
+}
+
+function initFixedBoard() {
+  fixedBlockArray = [];
+  for (let i = 0; i < BOARD_HEIGHT; ++i) {
+    fixedBlockArray.push([]);
+    for (let j = 0; j < BOARD_WIDTH; ++j) {
+      fixedBlockArray[i].push('');
+    }
+  }
+}
+
+function draw() {
+  drawBoard();
+  currentBlock.draw();
+}
+
+function drawScore() {
+  scoreElement.textContent = `Score: ${score}`;
 }
 
 function drawBoard() {
-  // board
   for (let i = 0; i < boardElement.length; ++i) {
     for (let j = 0; j < boardElement[i].length; ++j) {
       if (fixedBlockArray[i][j] !== '') {
@@ -249,171 +350,48 @@ function drawBoard() {
       }
     }
   }
-  // currentBlock
-  currentBlock.draw();
-}
-
-const KEY_SPACE = 32;
-const KEY_ARROW_LEFT = 37;
-const KEY_ARROW_UP = 38;
-const KEY_ARROW_RIGHT = 39;
-const KEY_ARROW_DOWN = 40;
-window.addEventListener('keydown', (e) => {
-  keyDown(e.keyCode);
-}, false);
-
-
-function keyDown(keyCode) {
-  // 원하는 방향으로 갈 수 있는가?
-
-  // 간다.
-  switch (keyCode) {
-    case KEY_ARROW_LEFT:
-      moveLeft();
-      break;
-    case KEY_ARROW_UP:
-      currentBlock.rotate();
-      break;
-    case KEY_ARROW_RIGHT:
-      moveRight();
-      break;
-    case KEY_ARROW_DOWN:
-      if (blockDown() === false) {
-        tryClearBlockLine(currentBlock.getBlockPosition().map(x => x[0]));
-        makeBlock();
-      };
-      break;
-    case KEY_SPACE:
-      while (true) {
-        if (blockDown() === false) {
-          tryClearBlockLine(currentBlock.getBlockPosition().map(x => x[0]));
-          makeBlock();
-          break;
-        };
-      }
-  }
-  drawBoard();
-}
-
-let timer;
-function run() {
-  timer = setInterval(() => {
-    if (blockDown() === false) {
-      makeBlock();
-    };
-    drawBoard();
-
-  }, 1000)
-}
-
-function moveSide(value) {
-  let blockPosition = currentBlock.getBlockPosition();
-  if (value === -1) {
-    if (Math.min(...blockPosition.map(x => x[1])) === 0) {
-      return;
-    }
-  } else {
-    if (Math.max(...blockPosition.map(x => x[1])) === BOARD_WIDTH -1) {
-      return;
-    }
-  }
-
-  for (let i = 0; i < blockPosition.length; ++i) {
-    if (fixedBlockArray[blockPosition[i][0]][blockPosition[i][1] + value] !== '') {
-      return;
-    }
-  }
-
-  currentBlock.posX += value;
-}
-
-function moveLeft() {
-  moveSide(-1);
-}
-
-function moveRight() {
-  moveSide(1);
-}
-
-
-
-function tryBlockDown() {
-  const blockPosition = currentBlock.getBlockPosition();
-  for (let i = 0; i < blockPosition.length; ++i) {
-    if (blockPosition[i][0] === BOARD_HEIGHT - 1) {
-      return false;
-    }
-    if (fixedBlockArray[blockPosition[i][0] + 1][blockPosition[i][1]] !== '') {
-      return false;
-    }
-  }
-  return true;
 }
 
 function blockDown() {
-  if (tryBlockDown()) {
-    // 내린다.
-    currentBlock.posY += 1;
-    return true;
-  } else {
+  if (currentBlock.blockDown() === false) {
     addFixedBlock(currentBlock);
+    tryClearBlockLine(currentBlock.getBlockPosition().map(x => x[0]));
+    makeBlock();
     return false;
+  };
+  return true;
+}
+
+const blockList = [BlockI, BlockT, BlockA, BlockB, BlockL, BlockR, BlockM];
+function makeBlock() {
+  const blockType = Math.floor(Math.random() * 7);
+  let block = new blockList[blockType];
+  currentBlock = block;
+  checkGameOver();
+}
+
+function checkGameOver() {
+  const blockPosition = currentBlock.getBlockPosition();
+  for (let i = 0; i < blockPosition.length; ++i) {
+    if (fixedBlockArray[blockPosition[i][0]][blockPosition[i][1]] !== '') {
+      alert('game Over');
+      restartGame();
+      return;
+    }
   }
 }
 
-// 회전 코드 수정 (좌우 막힌거, 블록막힌거)
-// 스코어 추가
-// 게임 종료
-// 블럭 색상 수정
-
-
-let currentBlock;
-function makeBlock() {
-  const blockType = Math.floor(Math.random() * 7);
-  let block;
-  switch (blockType){
-    case 0:
-      block = new BlockI();
-      break;
-    case 1:
-      block = new BlockT();
-      break;
-    case 2:
-      block = new BlockA();
-      break;
-    case 3:
-      block = new BlockB();
-      break;
-    case 4:
-      block = new BlockL();
-      break;
-    case 5:
-      block = new BlockR();
-      break;
-    case 6:
-      block = new BlockM();
-      break;
-  }
-  currentBlock = block;
+function restartGame() {
+  clearInterval(timer);
+  document.querySelector('.board').remove();
+  init();
 }
 
 function addFixedBlock(block) {
-  // block 의 좌표들을 가져온다.
-  // 가져온 좌표들을 셋팅한다.
-
   const blockPosition = block.getBlockPosition();
   if (blockPosition) {
     for (let i = 0; i < blockPosition.length; ++i) {
       fixedBlockArray[blockPosition[i][0]][blockPosition[i][1]] = block.color;
-    }
-  }
-}
-
-function initFixedBoard() {
-  for (let i = 0; i < BOARD_HEIGHT; ++i) {
-    fixedBlockArray.push([]);
-    for (let j = 0; j < BOARD_WIDTH; ++j) {
-      fixedBlockArray[i].push('');
     }
   }
 }
@@ -433,14 +411,26 @@ function tryClearBlockLine(yArray) {
       }
     }
   });
+
+  if (result.length > 0) {
+    score += result.length;
+    drawScore();
+  }
+}
+
+function run() {
+  timer = setInterval(() => {
+    blockDown();
+    draw();
+  }, GAME_SPEED);
 }
 
 function init() {
   initBoard();
+  initScore();
   initFixedBoard();
   makeBlock();
-  // addFixedBlock(new BlockI());
-  drawBoard();
+  draw();
   run();
 };
 
